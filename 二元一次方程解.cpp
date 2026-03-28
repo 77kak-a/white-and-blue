05a77b5e81973f9b0ccd0eef7da58ddc22ff2ba1
77kak-a <3343945195@qq.com>
2026-03-28 15:34:35 +0800

    Update 二元一次方程解.cpp

--- 二元一次方程解.cpp
+++ 二元一次方程解.cpp
@@ -7,18 +7,19 @@ int main(void)
 	int a,b,c;
 	double delta;
 	double x1,x2;
-	printf("请输入a，b，c的值，用空格分隔；")
+	printf("请输入a，b，c的值，用空格分隔；")；
 	scanf("%d%d%d",&a,&b,&c);
 
 	if (a==0)
 	{
-		printf("a不能为0，这不是二元一次方程。")
+		printf("a不能为0，这不是二元一次方程。")；
+			return 1;
 	}
 	delta = b*b - 4*a*c;
 	
 	if (delta >0)
 	{
-		x1 = (-b + sqrt(delta))/ (2.0*a));
+		x1 = (-b + sqrt(delta)) / (2.0*a));
 		x2 = (-b - sqrt(delta)) / (2.0*a));
 		printf("该一元二次方程有俩个解, x1 = %f, x2 = %f\n", x1, x2) ;
 	}
@@ -30,6 +31,6 @@ int main(void)
 	}
 	else
 	{
-		printf("无解") ;
+		printf("无实数解") ;
 	}
 }
